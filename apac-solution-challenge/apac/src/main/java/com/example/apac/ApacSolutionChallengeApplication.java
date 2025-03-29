fd759054442eb1e01120c33b61c3fb0731d40ae8
xxubin04 <yubinjeong40@gmail.com>
2025-03-29 18:20:49 +0900

    Chore: 기본 프로젝트 세팅

---
+++ apac-solution-challenge/apac/src/main/java/com/example/apac/ApacSolutionChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacSolutionChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacSolutionChallengeApplication.class, args);
+	}
+
+}
