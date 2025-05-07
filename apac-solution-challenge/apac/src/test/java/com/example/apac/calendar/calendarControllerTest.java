1775df30d5ea245310930d8f3ba375f36e281594
Cloie-Kim <sierra3144@naver.com>
2025-05-07 12:11:41 +0900

    Test: CalendarController 캘린더 조회 성공 테스트 추가
    MockMvc를 이용해 정상적인 JWT를 통한 캘린더 API 호출 시 예상 응답을 반환 시나리오 검증
    CalendarService는 mock 처리, JWT는 실제 secretKey 기반으로 생성하여 인증 흐름 테스트
    
    Related to: #4

---
+++ apac-solution-challenge/apac/src/test/java/com/example/apac/calendar/calendarControllerTest.java
@@ -0,0 +1,84 @@
+package com.example.apac.calendar;
+
+import com.example.apac.calendar.controller.CalendarController;
+import com.example.apac.calendar.dto.CalendarDay;
+import com.example.apac.calendar.dto.CalendarResponse;
+import com.example.apac.calendar.service.CalendarService;
+import com.example.apac.security.JwtUtil;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CalendarController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class calendarControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CalendarService calendarService;
+
+    @MockitoBean
+    private JwtUtil jwtUtil;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    @Test
+    @DisplayName("정상적인 JWT로 캘린더를 조회하면 캘린더 응답을 반환한다")
+    void getCalendar_returnExpextedResponse() throws Exception{
+        //given
+        String email="test@google.com";
+        int year=2025;
+        int month=5;
+
+
+        //when
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        String jwtToken = Jwts.builder()
+                .setSubject(email)
+                .signWith(key)
+                .compact();
+
+        CalendarResponse responseForTest= new CalendarResponse(
+                year, month,
+                List.of(
+                        new CalendarDay("2025-05-01", 3),
+                        new CalendarDay("2025-05-02", 2)
+                )
+        );
+
+        when(calendarService.getCalendar(email, year, month)).thenReturn(responseForTest);
+
+
+        //when&then
+        mockMvc.perform(get("/api/calendar")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtToken)
+                .param("year", "2025")
+                .param("month", "5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.year").value(2025))
+                .andExpect(jsonPath("$.month").value(5))
+                .andExpect(jsonPath("$.days[0].goalAchievedCount").value(3))
+                .andExpect(jsonPath("$.days[1].goalAchievedCount").value(2));
+    }
+
+}
