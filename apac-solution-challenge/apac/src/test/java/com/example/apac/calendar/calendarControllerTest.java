be9ff1c651d5acf6ab583752af5f3832fdbd7a8a
Cloie-Kim <sierra3144@naver.com>
2025-05-07 13:19:46 +0900

    Feat: Spring REST Docs 설정 추가
    restdocs를 사용하기 위한 의존성 및 설정을 추가
    API 명세 자동화 및 관리 효율성을 높이기 위한 기반 작업

--- apac-solution-challenge/apac/src/test/java/com/example/apac/calendar/calendarControllerTest.java
+++ apac-solution-challenge/apac/src/test/java/com/example/apac/calendar/calendarControllerTest.java
@@ -7,14 +7,19 @@ import com.example.apac.calendar.service.CalendarService;
 import com.example.apac.security.JwtUtil;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.crypto.SecretKey;
@@ -22,16 +27,23 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @WebMvcTest(CalendarController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 public class calendarControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private CalendarController calendarController;
 
     @MockitoBean
     private CalendarService calendarService;
@@ -41,6 +53,13 @@ public class calendarControllerTest {
 
     @Value("${jwt.secret.key}")
     private String secretKey;
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = standaloneSetup(calendarController)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
 
     @Test
     @DisplayName("정상적인 JWT로 캘린더를 조회하면 캘린더 응답을 반환한다")
@@ -78,7 +97,8 @@ public class calendarControllerTest {
                 .andExpect(jsonPath("$.year").value(2025))
                 .andExpect(jsonPath("$.month").value(5))
                 .andExpect(jsonPath("$.days[0].goalAchievedCount").value(3))
-                .andExpect(jsonPath("$.days[1].goalAchievedCount").value(2));
+                .andExpect(jsonPath("$.days[1].goalAchievedCount").value(2))
+                .andDo(document("calendar/get-calendar-success"));;
     }
 
     @Test
@@ -92,7 +112,8 @@ public class calendarControllerTest {
                         .header(HttpHeaders.AUTHORIZATION, invalidToken)
                         .param("year", "2025")
                         .param("month", "5"))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andDo(document("calendar/get-calendar-fail"));;
     }
 
 }
