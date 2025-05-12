0106aaa73203299169229c1894c442d89918ed56
xxubin04 <yubinjeong40@gmail.com>
2025-05-12 20:58:19 +0900

    Refactor: Swagger 명세서 수정

--- apac-solution-challenge/apac/src/main/java/com/example/apac/controller/GeminiController.java
+++ apac-solution-challenge/apac/src/main/java/com/example/apac/controller/GeminiController.java
@@ -70,12 +70,15 @@ package com.example.apac.controller;
 
 import com.example.apac.dto.ChatRequest;
 import com.example.apac.service.GeminiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatClient;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "GeminiController", description = "Gemini 챗봇 API")
 public class GeminiController {
 
     private final GeminiService geminiService;
@@ -83,7 +86,7 @@ public class GeminiController {
     public GeminiController(GeminiService geminiService) {
         this.geminiService = geminiService;
     }
-
+    @Operation(summary = "Gemini 응답 생성 및 DB 저장", description = "사용자에게 받은 요청에 따른 Gemini 응답을 생성합니다. 그리고 Firebase에 사용자의 요청과 Gemini 응답을 저장합니다. ")
     @PostMapping("/chat")
     public String chat(@RequestBody ChatRequest request) throws Exception {
         String prompt = request.getPrompt();
