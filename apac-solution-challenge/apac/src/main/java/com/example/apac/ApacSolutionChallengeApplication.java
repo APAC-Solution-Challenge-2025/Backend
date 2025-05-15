327d9cfd26645ae1dbeea8fb343a85855230b8b8
xxubin04 <yubinjeong40@gmail.com>
2025-05-16 06:17:49 +0900

    Refactor: 건강상태 저장 로직 수정

--- apac-solution-challenge/apac/src/main/java/com/example/apac/ApacSolutionChallengeApplication.java
+++ apac-solution-challenge/apac/src/main/java/com/example/apac/ApacSolutionChallengeApplication.java
@@ -1,5 +1,8 @@
 package com.example.apac;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +12,4 @@ public class ApacSolutionChallengeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApacSolutionChallengeApplication.class, args);
 	}
-
 }
